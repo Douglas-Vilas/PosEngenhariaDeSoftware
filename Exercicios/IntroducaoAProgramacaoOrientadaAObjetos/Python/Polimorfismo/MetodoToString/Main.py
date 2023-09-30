b07a268c6b575a4a04fa0d715369978a2b470ab8
Douglas Vilas <douglas_vilass@hotmail.com>
2023-09-30 17:19:41 -0300

    Método to String
    Método to String em Python.

---
+++ Exercicios/IntroducaoAProgramacaoOrientadaAObjetos/Python/Polimorfismo/MetodoToString/Main.py
@@ -0,0 +1,17 @@
+# Definindo as Strings
+primeira = "Java"
+segunda = "Csharp"
+terceira = "Python"
+
+# Imprimindo as operações
+print(primeira.upper())
+print(len(segunda))
+print(terceira)
+
+# Comparando a Primeira com a Segunda String
+result = primeira == segunda
+print("Primeira é igual a Segunda? ", result)
+
+# Comparando a Segunda com a Terceira String
+result = segunda == terceira
+print("Segunda é igual a Terceira? ", result)
