548c367cde7a9e0da39bedc3fc2edd5d2007fcf6
Douglas Vilas <douglas_vilass@hotmail.com>
2023-10-08 07:18:38 -0300

    Collections
    Linked List feitos em Java.

---
+++ Exercicios/IntroducaoAProgramacaoOrientadaAObjetos/Java/Collections/Linkedlist.java
@@ -0,0 +1,17 @@
+package Exercicios.IntroducaoAProgramacaoOrientadaAObjetos.Java.Collections;
+
+import java.util.*;
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        LinkedList<String> animais = new LinkedList<>();
+
+        animais.add("Cachorro");
+        animais.add("Cavalo");
+        animais.add("Vaca");
+        System.out.println("LinkedList: " + animais);
+
+        animais.add(1, "Gato");
+        System.out.println("LinkedList " + animais);
+    }
+}
