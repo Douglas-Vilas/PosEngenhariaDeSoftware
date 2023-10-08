8ba337d6bb9aa587f276414716d013113ef408da
Douglas Vilas <douglas_vilass@hotmail.com>
2023-10-08 10:58:39 -0300

    Collections
    Projeto com Lista feitos em Java.

---
+++ Exercicios/IntroducaoAProgramacaoOrientadaAObjetos/Java/Collections/ProjetoComListas.java
@@ -0,0 +1,25 @@
+package Exercicios.IntroducaoAProgramacaoOrientadaAObjetos.Java.Collections;
+
+import java.util.*;
+
+public class ProjetoComListas {
+    public static void main(String[] args) {
+        ArrayList<String> linguagens = new ArrayList<>();
+
+        linguagens.add("Java");
+        linguagens.add("CSharp");
+        linguagens.add("Python");
+        linguagens.add("JavaScript");
+
+        System.out.println("ArrayList: " + linguagens);
+
+        String[] arr = new String[linguagens.size()];
+
+        linguagens.toArray(arr);
+        System.out.println("Array: ");
+
+        for (String item:arr) {
+            System.out.println(item + ", ");
+        }
+    }
+}
