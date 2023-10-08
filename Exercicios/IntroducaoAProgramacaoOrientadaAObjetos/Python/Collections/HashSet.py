217a9362ff8630d17d138ec2257948a28c509a6b
Douglas Vilas <douglas_vilass@hotmail.com>
2023-10-08 09:21:37 -0300

    Collections
    HashSet feito em Python.

---
+++ Exercicios/IntroducaoAProgramacaoOrientadaAObjetos/Python/Collections/HashSet.py
@@ -0,0 +1,11 @@
+# Cria um Set vazio, chamado 'numeros'
+numeros = set()
+
+# Adiciona elementos ao set.
+numeros.add(2)
+numeros.add(5)
+numeros.add(8)
+
+# Itera sobre o set e imprime cada elemento. 
+for numero in numeros:
+    print(numero)
