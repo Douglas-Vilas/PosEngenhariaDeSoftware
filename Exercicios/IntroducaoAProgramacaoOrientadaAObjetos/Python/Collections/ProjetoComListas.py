dda055cb09ee28877d549f3a4af620d6cf2f7f18
Douglas Vilas <douglas_vilass@hotmail.com>
2023-10-08 11:08:55 -0300

    Collections
    Projeto com Listas, feito em Python.

---
+++ Exercicios/IntroducaoAProgramacaoOrientadaAObjetos/Python/Collections/ProjetoComListas.py
@@ -0,0 +1,13 @@
+# Cria uma lista chamada 'linguagens'
+linguagens = ["Java", "CSharp", "Python", "JavaScript"]
+
+# Imprime a lista 'linguagens'
+print("Lista: ", linguagens)
+
+# Cria uma variável 'arr' que é igual à lista 'linguagens'
+arr = linguagens
+print("Array: ")
+
+# Loop que percorrer cada elemento da lista 'arr'
+for item in arr:
+    print(item + ", ")
