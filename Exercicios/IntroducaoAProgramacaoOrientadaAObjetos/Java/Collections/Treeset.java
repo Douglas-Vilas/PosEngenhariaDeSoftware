8a8d0d1ceb3be81bc294168ca52d505e03f3e021
Douglas Vilas <douglas_vilass@hotmail.com>
2023-10-08 10:08:46 -0300

    Collections
    TreeSet feito em Java

---
+++ Exercicios/IntroducaoAProgramacaoOrientadaAObjetos/Java/Collections/Treeset.java
@@ -0,0 +1,23 @@
+package Exercicios.IntroducaoAProgramacaoOrientadaAObjetos.Java.Collections;
+
+import java.util.*;
+
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<Integer> numeros = new TreeSet();
+
+        numeros.add(8);
+        numeros.add(6);
+        numeros.add(9);
+
+        System.out.println("TreeSet: " + numeros);
+
+        boolean result = numeros.remove(6);
+        System.out.println("Removeu? " + result);
+        System.out.println("TreeSet: " + numeros);
+
+        result = numeros.removeAll(numeros);
+        System.out.println("Removeu todos? " + result);
+        System.out.println("TreeSet: " + numeros);
+    }
+}
