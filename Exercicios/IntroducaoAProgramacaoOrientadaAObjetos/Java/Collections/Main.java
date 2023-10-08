2f319a10ad9436f688fed28acf15b24ea41c2d0d
Douglas Vilas <douglas_vilass@hotmail.com>
2023-10-08 06:27:16 -0300

    Collections
    Iterador e Comparator feitos em Java.

---
+++ Exercicios/IntroducaoAProgramacaoOrientadaAObjetos/Java/Collections/Main.java
@@ -0,0 +1,25 @@
+package Exercicios.IntroducaoAProgramacaoOrientadaAObjetos.Java.Collections;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+
+        System.out.println("ArrayList: " + numeros);
+
+        // Criando uma instância de Iterator
+        Iterator<Integer> it = numeros.iterator();
+
+        int numero = it.next();
+
+        System.out.println("Elemento: " + numero);     
+        
+        while(it.hasNext()){
+            it.forEachRemaining((value) -> System.out.print(value + ", "));
+        }
+    }
+}
